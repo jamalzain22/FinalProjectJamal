2d80dc3269154a5d651eb3bb99710df5bb09959f
jamalzain22 <jama4650@mylaurier.ca>
2015-12-01 20:16:44 -0500

    Update impress.js

--- impress.js
+++ impress.js
@@ -1,3 +1,3 @@
 <div id="Turning Test" class="step slide" data-x="-1000" data-y"-1500"></div>
-  <q>The Turning Test</q>
-  <img src="http://csunplugged.org/wp-content/uploads/2015/03/turing-test.jpg1246936875"
+<q>The Turning Test</q>
+<img src="http://csunplugged.org/wp-content/uploads/2015/03/turing-test.jpg1246936875"
