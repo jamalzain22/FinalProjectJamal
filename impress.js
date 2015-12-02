b5d22787f5c2644e022d3d4dceb3eba7e8667321
jamalzain22 <jama4650@mylaurier.ca>
2015-12-01 20:36:08 -0500

    Update impress.js

--- impress.js
+++ impress.js
@@ -1,3 +1,4 @@
 <div id="Turning Test" class="step slide" data-x="-1000" data-y"-1500"></div>
 <q>The Turning Test</q>
 <img src="http://csunplugged.org/wp-content/uploads/2015/03/turing-test.jpg1246936875" alt=" Final Project Jamal" title="Final Project Jamal" style="width:100%" />
+
